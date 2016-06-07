7faf62b1a6113f373926ae946b689b7219661926
Leaven <503807878@qq.com>
2016-06-07 17:39:08 +0800

    实现功能

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,13 @@
 # -*- encoding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from .forms import PostForm
 def post_list(request):
     posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
